63fd488df26bef325e0782478a1c996530c13e21
Andrei10Toma <andreiitomaa@gmail.com>
2021-03-15 00:32:29 +0200

    Queen implementation/Better gameboard print

---
+++ pieces/Queen.cpp
@@ -0,0 +1,82 @@
+#include "Queen.hpp"
+#include <vector>
+#include "../GameBoard.hpp"
+#include <iostream>
+using namespace std;  
+	
+Queen::Queen(pair<int, char> pos, bool color) : Piece(pos, color){
+}
+
+bool inTableQueen(int x, char y, GameBoard* gameBoard) {
+    if (x >= 9 || x <= 0 || y < 'a' || y > 'h') {
+        return false;
+    }
+    if (gameBoard->table[x][y - 'a' + 1] != NULL && gameBoard->table[x][y - 'a' + 1]->color == 1) {
+        return false;
+    }
+    return true;
+}
+
+vector<pair<int, char>> Queen::findPositions(GameBoard* gameBoard) {
+    vector<pair<int, char>> possiblePositions;
+    if (this -> color == 1) {
+        // check upper line
+        for (int i = this->position.first - 1; i >= 1; i--) {
+            if (gameBoard->table[i][this->position.second - 'a' + 1] == NULL) {
+                possiblePositions.push_back(make_pair(i, this->position.second));
+            }
+            else {
+                break;
+            }
+        }
+        // check lower line
+        for (int i = this->position.first + 1; i <= 8; i++) {
+            if (gameBoard->table[i][this->position.second - 'a' + 1] == NULL) {
+                possiblePositions.push_back(make_pair(i, this->position.second));
+            }
+            else {
+                break;
+            }
+        }
+        // check left line
+        for (int i = this->position.second + 1; i <= 'h'; i++) {
+            if (gameBoard->table[this->position.first][i - 'a' + 1] == NULL) {
+                possiblePositions.push_back(make_pair(this->position.first, i));
+            }
+            else {
+                break;
+            }
+        }
+        // check right line
+        for (int i = this->position.second - 1; i >= 'a'; i--) {
+            if (gameBoard->table[this->position.first][i - 'a' + 1] == NULL) {
+                possiblePositions.push_back(make_pair(this->position.first, i));
+            }
+            else {
+                break;
+            }
+        }
+        // check superior main diagonal
+        int i; char j;
+        for (i = this->position.first - 1, j = this->position.second - 1; i >= 1 && j >= 'a'; i--, j--) {
+            if (gameBoard->table[i][j - 'a' + 1] == NULL) {
+                possiblePositions.push_back(make_pair(i, j));
+            }
+            else {
+                break;
+            }
+        }
+        // check inferior main diagonal
+        for (i = this->position.first + 1, j = this->position.second + 1; i <= 8 && j <= 'h'; i++, j++) {
+            if (gameBoard->table[i][j - 'a' + 1] == NULL) {
+                possiblePositions.push_back(make_pair(i, j));
+            }
+            else {
+                break;
+            }
+        }
+        // check superior second diagonal
+        // check inferior second diagonal
+    }
+    return possiblePositions;
+}	
---
+++ pieces/Queen.hpp
@@ -0,0 +1,16 @@
+#ifndef QUEEN_H
+#define QUEEN_H
+#pragma once
+#include "Piece.hpp"
+#include <vector>
+	
+class Queen : public Piece
+{
+	private:
+
+	public:
+		vector<pair<int, char>> findPositions(GameBoard* gameBoard);
+		Queen(pair<int, char> pos, bool color);
+
+};
+#endif
